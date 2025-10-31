5229c7494fea12d00f3a8515f073da3f22a667d8
n-j-p <10066183+n-j-p@users.noreply.github.com>
2025-10-31 17:41:02 +1100

    added model

--- kaggle_titanic_repo_example/modelling.py
+++ kaggle_titanic_repo_example/modelling.py
@@ -1,2 +1,4 @@
-import kaggle_titanic_repo_example.dataprep
+import kaggle_titanic_repo_example.param
+from sklearn.linear_model import LogisticRegression
 
+model = LogisticRegression(C = kaggle_titanic_repo_example.param.lr_C)
